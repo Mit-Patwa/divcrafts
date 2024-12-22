886761b6aec4790705e57f25b41df458a6f626af
Mit-Patwa <mitdpatwa@gmail.com>
2024-12-22 17:46:47 +0530

    added initial files

---
+++ table2.js
@@ -0,0 +1,380 @@
+var timetables = {
+    "9A": [
+        {
+            time: "9:30 - 10:10",
+            Monday: "KMV",
+            Tuesday: "KMV",
+            Wednesday: "KMV",
+            Thursday: "KMV",
+            Friday: "KMV",
+        },
+        {
+            time: "10:10 - 10:50",
+            Monday: "SMB",
+            Tuesday: "SMB",
+            Wednesday: "SMB",
+            Thursday: "GS",
+            Friday: "UMD",
+        },
+        {
+            time: "10:50 - 11:30",
+            Monday: "SMS",
+            Tuesday: "SMS",
+            Wednesday: "SMS",
+            Thursday: "UMD",
+            Friday: "SMS",
+
+        },
+        {
+            time: "11:30 - 11:40",
+            Monday: "Short Break",
+            Tuesday: "Short Break",
+            Wednesday: "Short Break",
+            Thursday: "Short Break",
+            Friday: "Short Break",
+
+        },
+        {
+            time: "11:40 - 12:20",
+            Monday: "UMD",
+            Tuesday: "UMD",
+            Wednesday: "VB",
+            Thursday: "SMS",
+            Friday: "HS",
+
+        },
+        {
+            time: "12:20 - 1:00",
+            Monday: "SG",
+            Tuesday: "GS",
+            Wednesday: "SG",
+            Thursday: "SMB",
+            Friday: "SMB",
+
+        },
+        {
+            time: "1:00 - 1:30",
+            Monday: "",
+            Tuesday: "Lunch Break",
+            Wednesday: "Lunch Break",
+            Thursday: "Lunch Break",
+            Friday: "Lunch Break",
+
+        },
+        {
+            time: "1:30 - 2:10",
+            Monday: "VB",
+            Tuesday: "COMP",
+            Wednesday: "GS",
+            Thursday: "SG",
+            Friday: "GS",
+
+        },
+        {
+            time: "2:10 - 2:50",
+            Monday: "HS",
+            Tuesday: "SG",
+            Wednesday: "UMD",
+            Thursday: "HM",
+            Friday: "SG",
+
+        },
+        {
+            time: "2:50 - 3:30",
+            Monday: "HM",
+            Tuesday: "KMV",
+            Wednesday: "SG",
+            Thursday: "VB",
+            Friday: "CCE",
+
+        },
+
+        // ... more rows for Class 1
+    ],
+    "9B": [
+        {
+            time: "9:30 - 10:10",
+            Monday: "UMD",
+            Tuesday: "UMD",
+            Wednesday: "UMD",
+            Thursday: "UMD",
+            Friday: "UMD",
+
+        },
+        {
+            time: "10:10 - 10:50",
+            Monday: "GS",
+            Tuesday: "HS",
+            Wednesday: "VB",
+            Thursday: "VB",
+            Friday: "VB",
+
+        },
+        {
+            time: "10:50 - 11:30",
+            Monday: "SG",
+            Tuesday: "SG",
+            Wednesday: "SG",
+            Thursday: "SG",
+            Friday: "SG",
+
+        },
+        {
+            time: "11:30 - 11:40",
+            Monday: "Short Break",
+            Tuesday: "Short Break",
+            Wednesday: "Short Break",
+            Thursday: "Short Break",
+            Friday: "Short Break",
+
+        },
+        {
+            time: "11:40 - 12:20",
+            Monday: "SMS",
+            Tuesday: "KMV",
+            Wednesday: "GS",
+            Thursday: "HS",
+            Friday: "SMV",
+
+        },
+        {
+            time: "12:20 - 1:00",
+            Monday: "SMV",
+            Tuesday: "SMS",
+            Wednesday: "SMS",
+            Thursday: "GS",
+            Friday: "GS",
+
+        },
+        {
+            time: "1:00 - 1:30",
+            Monday: "Lunch Break",
+            Tuesday: "Lunch Break",
+            Wednesday: "Lunch Break",
+            Thursday: "Lunch Break",
+            Friday: "Lunch Break",
+
+        },
+        {
+            time: "1:30 - 2:10",
+            Monday: "KMV",
+            Tuesday: "GS",
+            Wednesday: "KMV",
+            Thursday: "SMS",
+            Friday: "KMV",
+
+        },
+        {
+            time: "2:10 - 2:50",
+            Monday: "UMD",
+            Tuesday: "SMB",
+            Wednesday: "SG",
+            Thursday: "HM",
+            Friday: "SMS",
+
+        },
+        {
+            time: "2:50 - 3:30",
+            Monday: "HM",
+            Tuesday: "COMP",
+            Wednesday: "HS",
+            Thursday: "SMS",
+            Friday: "CCE",
+
+        },
+
+        // ... more rows for Class 1
+    ],
+    "10A": [
+        {
+            time: "9:30 - 10:10",
+            Monday: "GS",
+            Tuesday: "GS",
+            Wednesday: "GS",
+            Thursday: "GS",
+            Friday: "GS",
+
+        },
+        {
+            time: "10:10 - 10:50",
+            Monday: "SG",
+            Tuesday: "SG",
+            Wednesday: "SG",
+            Thursday: "SG",
+            Friday: "SG",
+
+        },
+        {
+            time: "10:50 - 11:30",
+            Monday: "UMD",
+            Tuesday: "UMD",
+            Wednesday: "UMD",
+            Thursday: "SMS",
+            Friday: "UMD",
+
+        },
+        {
+            time: "11:30 - 11:40",
+            Monday: "Short Break",
+            Tuesday: "Short Break",
+            Wednesday: "Short Break",
+            Thursday: "Short Break",
+            Friday: "Short Break",
+
+        },
+        {
+            time: "11:40 - 12:20",
+            Monday: "KMV",
+            Tuesday: "SMS",
+            Wednesday: "KMV",
+            Thursday: "KMV",
+            Friday: "SMS",
+
+        },
+        {
+            time: "12:20 - 1:00",
+            Monday: "SMS",
+            Tuesday: "KMV",
+            Wednesday: "SMB",
+            Thursday: "UMD",
+            Friday: "KMV",
+
+        },
+        {
+            time: "1:00 - 1:30",
+            Monday: "Lunch Break",
+            Tuesday: "Lunch Break",
+            Wednesday: "Lunch Break",
+            Thursday: "Lunch Break",
+            Friday: "Lunch Break",
+
+        },
+        {
+            time: "1:30 - 2:10",
+            Monday: "SMB",
+            Tuesday: "SMB",
+            Wednesday: "STEM",
+            Thursday: "GS",
+            Friday: "SMV",
+
+        },
+        {
+            time: "2:10 - 2:50",
+            Monday: "VB",
+            Tuesday: "HM",
+            Wednesday: "SMS",
+            Thursday: "COMP",
+            Friday: "HM",
+
+        },
+        {
+            time: "2:50 - 3:30",
+            Monday: "HS",
+            Tuesday: "VB",
+            Wednesday: "VB",
+            Thursday: "HS",
+            Friday: "CCE",
+
+        },
+
+        // ... more rows for Class 1
+    ],
+    "10B": [
+        {
+            time: "9:30 - 10:10",
+            Monday: "SMS",
+            Tuesday: "SMS",
+            Wednesday: "SMS",
+            Thursday: "SMS",
+            Friday: "SMS",
+
+        },
+        {
+            time: "10:10 - 10:50",
+            Monday: "KMV",
+            Tuesday: "KMV",
+            Wednesday: "KMV",
+            Thursday: "KMV",
+            Friday: "KMV",
+
+        },
+        {
+            time: "10:50 - 11:30",
+            Monday: "GS",
+            Tuesday: "GS",
+            Wednesday: "GS",
+            Thursday: "GS",
+            Friday: "GS",
+
+        },
+        {
+            time: "11:30 - 11:40",
+            Monday: "Short Break",
+            Tuesday: "Short Break",
+            Wednesday: "Short Break",
+            Thursday: "Short Break",
+            Friday: "Short Break",
+
+        },
+        {
+            time: "11:40 - 12:20",
+            Monday: "SMB",
+            Tuesday: "SMB",
+            Wednesday: "SMB",
+            Thursday: "SMB",
+            Friday: "UMD",
+
+        },
+        {
+            time: "12:20 - 1:00",
+            Monday: "LIB",
+            Tuesday: "VB",
+            Wednesday: "VB",
+            Thursday: "SG",
+            Friday: "SG",
+
+        },
+        {
+            time: "1:00 - 1:30",
+            Monday: "Lunch Break",
+            Tuesday: "Lunch Break",
+            Wednesday: "Lunch Break",
+            Thursday: "Lunch Break",
+            Friday: "Lunch Break",
+
+        },
+        {
+            time: "1:30 - 2:10",
+            Monday: "UMD",
+            Tuesday: "UMD",
+            Wednesday: "UMD",
+            Thursday: "UMD",
+            Friday: "STEM",
+
+        },
+        {
+            time: "2:10 - 2:50",
+            Monday: "SG",
+            Tuesday: "HM",
+            Wednesday: "SG",
+            Thursday: "SMS",
+            Friday: "HM",
+
+        },
+        {
+            time: "2:50 - 3:30",
+            Monday: "VB",
+            Tuesday: "SG",
+            Wednesday: "SMS",
+            Thursday: "COMP",
+            Friday: "CCE",
+
+        },
+
+        // ... more rows for Class 1
+    ],
+};
+
+
+// Alternatively, you can use forEach for a more concise syntax:
+// Object.values(timetables).forEach(classTimetables => classTimetables.forEach(entry => delete entry.Saturday));
